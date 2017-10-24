e9d750cbe446b1c2bd20abe3a336e9999e6c0237
Grzegorz Hudziak <hudziakgrzegorz@gmail.com>
2017-10-24 14:05:44 +0200

    Add customization example

--- ALFader/ViewController.swift
+++ ALFader/ViewController.swift
@@ -12,20 +12,28 @@ class ViewController: UIViewController, FaderDelegate {
     let label1 = UILabel()
     let label2 = UILabel()
     
-    let fader = Fader(frame: UIScreen.main.bounds.insetBy(dx: 32, dy: UIScreen.main.bounds.height / 2.3))
+    let fader = Fader()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        label1.frame = CGRect(x: 0, y: 0, width: 400, height: 50)
-        label2.frame = CGRect(x: 0, y: 50, width: 400, height: 50)
+        label1.frame = CGRect(x: 0, y: 50, width: 400, height: 50)
+        label2.frame = CGRect(x: 0, y: 100, width: 400, height: 50)
+        
         
         fader.delegate = self
-        fader.initLeftValue = 24
-        fader.initRightValue = 7
+        fader.leftThumbBackgroundColor = UIColor.orange
+        fader.rightThumbBackgroundColor = UIColor.orange
+        fader.rangeLineColor = UIColor.orange
+        fader.lineHeight = 6
+        fader.thumbRadius = 15
+
+        fader.frame = UIScreen.main.bounds.insetBy(dx: 32, dy: UIScreen.main.bounds.height / 2.3)
+        fader.initLeftValue = 3
+        fader.initRightValue = 15
+        
         view.addSubview(fader)
         view.addSubview(label1)
         view.addSubview(label2)
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     func rangeDidChage(left: CGFloat, right: CGFloat) {
